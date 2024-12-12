c746f104468598b680e6a469f1e89b2ec523bb81
eunice-hong <dev@eunice-hong.com>
2024-12-12 23:14:44 +0900

    solution: solve Day 9 Part 1

--- eunice-hong/src/test/kotlin/com/eunicehong/Day09Test.kt
+++ eunice-hong/src/test/kotlin/com/eunicehong/Day09Test.kt
@@ -10,8 +10,8 @@ internal class Day09Test : DailyTest(9) {
     private val testDay09: Day09 = Day09()
 
     @Test
-     fun puzzleSolution1Example() {
-         val exampleInput = "2333133121414131402"
+    fun puzzleSolution1Example() {
+        val exampleInput = "2333133121414131402"
         val expected = "1928"
         val actual = testDay09.solution1(exampleInput)
         assertEquals(expected, actual, "🎅 Solution for Example 1 Day $dayString has not been implemented yet.")
@@ -24,6 +24,15 @@ internal class Day09Test : DailyTest(9) {
         val actual = testDay09.solution1(input)
         assertEquals(expected, actual, "🎅 Solution for Puzzle 1 Day $dayString has not been implemented yet.")
         println("🌲 Day $dayString Puzzle 1 completed! $actual")
+    }
+
+    @Test
+    fun puzzleSolution2Example() {
+        val exampleInput = "2333133121414131402"
+        val expected = "2858"
+        val actual = testDay09.solution2(exampleInput)
+        assertEquals(expected, actual, "🎅 Solution 2 for Example 1 Day $dayString has not been implemented yet.")
+        println("🌲 Day $dayString Example 1 completed! $actual")
     }
 
     @Test
