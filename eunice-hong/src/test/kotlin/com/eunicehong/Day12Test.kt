e4a2a5eb89d53bb503abda5c043a1662f9014237
eunice-hong <dev@eunice-hong.com>
2024-12-12 23:05:38 +0900

    chore: add puzzle input and description

--- eunice-hong/src/test/kotlin/com/eunicehong/Day12Test.kt
+++ eunice-hong/src/test/kotlin/com/eunicehong/Day12Test.kt
@@ -10,6 +10,58 @@ internal class Day12Test : DailyTest(12) {
     private val testDay12: Day12 = Day12()
 
     @Test
+    fun puzzleSolution1Example1() {
+        val exampleInput =
+            """
+            AAAA
+            BBCD
+            BBCC
+            EEEC
+            """.trimIndent()
+        val expected = "140"
+        val actual = testDay12.solution1(exampleInput)
+        assertEquals(expected, actual, "🎅 Solution for Example 1 Day $dayString has not been implemented yet.")
+        println("🌲 Day $dayString Puzzle 1 completed! $actual")
+    }
+
+    @Test
+    fun puzzleSolution1Example2() {
+        val exampleInput =
+            """
+            OOOOO
+            OXOXO
+            OOOOO
+            OXOXO
+            OOOOO
+            """.trimIndent()
+        val expected = "772"
+        val actual = testDay12.solution1(exampleInput)
+        assertEquals(expected, actual, "🎅 Solution for Example @ Day $dayString has not been implemented yet.")
+        println("🌲 Day $dayString Puzzle 1 completed! $actual")
+    }
+
+    @Test
+    fun puzzleSolution1Example3() {
+        val exampleInput =
+            """
+            RRRRIICCFF
+            RRRRIICCCF
+            VVRRRCCFFF
+            VVRCCCJFFF
+            VVVVCJJCFE
+            VVIVCCJJEE
+            VVIIICJJEE
+            MIIIIIJJEE
+            MIIISIJEEE
+            MMMISSJEEE
+            """.trimIndent()
+        val expected = "1930"
+        val actual = testDay12.solution1(exampleInput)
+        assertEquals(expected, actual, "🎅 Solution for Example 3 Day $dayString has not been implemented yet.")
+        println("🌲 Day $dayString Puzzle 1 completed! $actual")
+    }
+
+    @Test
     override fun puzzleSolution1() {
         val expected = "Not implemented"
         val actual = testDay12.solution1(input)
