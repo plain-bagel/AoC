ed67b0ea5ef91b6f489e4b49da47392dc7e7988e
eunice-hong <dev@eunice-hong.com>
2024-12-13 19:39:03 +0900

    chore: add puzzle input and description

--- eunice-hong/src/test/kotlin/com/eunicehong/Day13Test.kt
+++ eunice-hong/src/test/kotlin/com/eunicehong/Day13Test.kt
@@ -10,6 +10,32 @@ internal class Day13Test : DailyTest(13) {
     private val testDay13: Day13 = Day13()
 
     @Test
+    fun puzzleSolution1Example() {
+        val exampleInput =
+            """
+            Button A: X+94, Y+34
+            Button B: X+22, Y+67
+            Prize: X=8400, Y=5400
+
+            Button A: X+26, Y+66
+            Button B: X+67, Y+21
+            Prize: X=12748, Y=12176
+
+            Button A: X+17, Y+86
+            Button B: X+84, Y+37
+            Prize: X=7870, Y=6450
+
+            Button A: X+69, Y+23
+            Button B: X+27, Y+71
+            Prize: X=18641, Y=10279
+            """.trimIndent()
+        val expected = "480"
+        val actual = testDay13.solution1(exampleInput)
+        assertEquals(expected, actual, "🎅 Solution for Puzzle 1 Day $dayString has not been implemented yet.")
+        println("🌲 Day $dayString Puzzle 1 completed! $actual")
+    }
+
+    @Test
     override fun puzzleSolution1() {
         val expected = "Not implemented"
         val actual = testDay13.solution1(input)
